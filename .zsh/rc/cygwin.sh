46410585252f3a69bf78650ed49cff3469ea1bdd
Kovensky <diogomfranco@gmail.com>
2012-10-20 11:44:06 +0000

    Set ulimit -c 0 on cygwin
    Stops it from creating .stackdump files every time a program aborts.

--- .zsh/rc/cygwin.sh
+++ .zsh/rc/cygwin.sh
@@ -15,4 +15,6 @@ if [[ "`uname`" =~ "CYGWIN" ]]; then
     _wrap_exe_if_exists /cygdrive/c/Program*/VideoLAN/VLC/vlc.exe(N)
     _wrap_exe_bg_if_exists /cygdrive/c/Program*/Sublime\ Text\ 2/sublime_text.exe(N)
     alias st=sublime_text
+
+    ulimit -c 0
 fi
