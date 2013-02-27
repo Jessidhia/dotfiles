6b8ad282abe33667911855c09b74c62611f9dfe4
Kovensky <diogomfranco@gmail.com>
2013-02-27 13:01:50 +0000

    Support adding .git/info/exclude info in submodules
    Used for "unregistered submodules" created by bin/update-subrepos.sh.
    Use the feature to ignore zsh-syntax-highlighting, since it caused
    .oh-my-zsh to be always marked as modified.

--- bin/update-subrepos.sh
+++ bin/update-subrepos.sh
@@ -2,10 +2,16 @@
 clone_or_pull () {
     git_src="$1"
     clone_dst="$2"
+    module_name="$3"
 
     if ! [ -d "$clone_dst" ]; then
         mkdir -p "$(dirname "$clone_dst")"
         git clone "$git_src" "$clone_dst"
+
+        if [ -n "$module_name" ]; then
+            in_module_path="$(echo "$2" | sed "s!.*$module_name/!!")"
+            echo "$in_module_path" >> "$HOME/.git/modules/$module_name/info/exclude"
+        fi
     else
         pushd "$clone_dst"
         git pull
@@ -14,5 +20,4 @@ clone_or_pull () {
 }
 
 clone_or_pull git://github.com/sstephenson/ruby-build.git            "$HOME/.rbenv/plugins/ruby-build"
-clone_or_pull git://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting"
-
+clone_or_pull git://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting" ".oh-my-zsh"
