b82e6d170d3ccb7f23ec28b4ea6ccdfc76d63647
Jessica <jessica@jessidhia.dev>
2019-11-27 12:57:04 +0900

    Check minimum go version before trying to install moar

--- dirs/zsh/rc/pager.sh
+++ dirs/zsh/rc/pager.sh
@@ -1,4 +1,5 @@
-if [[ -z $commands[moar] && -n $commands[go] ]]; then
+if [[ -z $commands[moar] && -n $commands[go] ]] &&
+  is-at-least 1.9.0 "$(go version | cut -d' ' -f3 | sed s/go//)"; then
   ! go get -v github.com/walles/moar
   hash -r
 fi
