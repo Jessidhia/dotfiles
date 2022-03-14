5a9f1b7809cb53b7ac0fbae4a0dcd76655fd959f
Jessica <jessica@jessidhia.dev>
2022-03-14 16:59:44 +0900

    Workaround for vscode bug in Wayland

--- dirs/zsh/rc/editor.sh
+++ dirs/zsh/rc/editor.sh
@@ -1,5 +1,11 @@
 export EDITOR="emacs -nw"
 
 if which code &>/dev/null; then
-    export VISUAL="code -w"
+    if [[ -n $WAYLAND_DISPLAY ]]; then
+        # workaround for Electron rendering incorrectly in Wayland
+        alias code='code --enable-features=Vulkan'
+        export VISUAL="code --enable-features=Vulkan -w"
+    else
+        export VISUAL="code -w"
+    fi
 fi
