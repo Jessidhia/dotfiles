2d4c77dbceae973debc6e500d63a8328bfb4c58c
Jessica <jessica@jessidhia.dev>
2022-05-23 21:06:38 +0900

    Add a way to install the macos compose key dictionary

--- dirs/zsh/rc/macos.sh
+++ dirs/zsh/rc/macos.sh
@@ -1,4 +1,21 @@
 if [[ "$OSTYPE" = darwin* ]]; then
     export PKG_CONFIG_LIBDIR="/usr/local/lib/pkgconfig:/opt/X11/lib/pkgconfig:/usr/lib/pkgconfig"
-    #    export SSL_CERT_FILE="/usr/local/etc/openssl/ca-bundle.crt"
+    #export SSL_CERT_FILE="/usr/local/etc/openssl/ca-bundle.crt"
+
+    if [[ -d "$HOME"/.homesick/repos/dotfiles ]]; then
+        # manually install files from dirs/Library
+        # homeshick can't manage them well, at least not without polutting non-macos homes
+        local Library="$HOME"/.homesick/repos/dotfiles/dirs/Library
+        .library_install() {
+            local folder_name="$1"
+
+            install -d "$HOME"/Library/"$folder_name"
+            install -Cp "$Library"/"$folder_name"/* "$HOME"/Library/"$folder_name"
+        }
+
+        .library_install KeyBindings
+
+        unset Library
+        unset -f .library_install
+    fi
 fi
